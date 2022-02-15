586187dcdf630a8d6b889d8f5b948720e8eb37cf
jibon01 <chyjibo556@gmail.com>
2022-02-15 12:56:15 +0600

    Baper Bank

---
+++ Bank.js
@@ -0,0 +1,16 @@
+document.getElementById('login-button').addEventListener('click', function () {
+
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    // check email and password
+    if (userEmail == 'baper@sontan.com' && userPassword == 'secret') {
+        window.location.href = 'Banking.html'
+    }
+    else {
+        console.log('Not mathced');
+    };
+})
